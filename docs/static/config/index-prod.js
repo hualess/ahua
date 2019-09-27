c624fa3d6c94935e106985677bfcfd17d69b450b
liubaohua <15093000951@163.com>
2019-09-27 09:15:15 +0800

    vue build

---
+++ docs/static/config/index-prod.js
@@ -0,0 +1,9 @@
+/**
+ * 生产环境
+ */
+;(function () {
+  window.SITE_CONFIG = {}
+  // api接口请求地址
+  window.SITE_CONFIG['baseUrl'] = '/static/json'
+  // window.SITE_CONFIG['baseUrl'] = 'http://182.254.171.61:8080/dbblog/'
+})()
