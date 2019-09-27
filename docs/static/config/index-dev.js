7252fd7668a875e6befbf947ad6aabb6434631da
liubaohua <15093000951@163.com>
2019-09-27 13:34:21 +0800

    vue build

---
+++ docs/static/config/index-dev.js
@@ -0,0 +1,9 @@
+/**
+ * 开发环境
+ */
+;(function () {
+  window.SITE_CONFIG = {}
+  // api接口请求地址
+  window.SITE_CONFIG['baseUrl'] = '/static/json'
+  // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/dbblog/'
+})()
